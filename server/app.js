ce8c67b9a4078693c9e92a7c0ed4b8cc8ad6c073
Timothy Tse <timothy.sc.tse@gmail.com>
2016-11-10 10:40:07 -0500

    Change path to reflect local environment to access the correct path.
    All routes that do not exist will return status 404 error

--- server/app.js
+++ server/app.js
@@ -7,9 +7,13 @@ var config = require('./config');
 var app = express();
 var server = require('http').createServer(app);
 
-app.set('appPath', path.join(config.root, 'client'));
+app.set('appPath', path.join(config.root, 'TimmyC/scenario-tim'));
 app.set('view engine', 'html');
 require('./routes')(app);
+app.use(function(req, res) {
+    res.status(400);
+    res.send('404: Page not Found', 404);
+ });
 
 server.listen(config.port, config.ip, function () {
     console.log('Express server listening on %d, in %s mode', config.port, app.get('env'));
