d08fe268c2bc01c2dcca82e672b9faae1aef278b
Timothy Tse <timothy.sc.tse@gmail.com>
2016-11-10 10:36:11 -0500

    Add two routes and hard coded object data.
    One route can access entire list of data
    Second route can query an ID to and get back the name of booking.

--- server/routes.js
+++ server/routes.js
@@ -1,10 +1,32 @@
 'use strict';
 
 var path = require('path');
+var bookings = {
+  1: "Asia",
+  2: "Europe",
+  3: "Africa",
+  4: "Australia",
+  5: "South America",
+  6: "North America",
+  7: "Antarica"
+};
 
 module.exports = function (app) {
-    app.route('/*')
+    app.route('/')
         .get(function (req, res) {
-            res.sendFile(path.resolve(app.get('appPath') + '/index.html'));
+            res.sendFile(path.resolve(app.get('appPath') + '/app/index.html'));
         });
+    app.get('/booking/list', (req, res) => {
+      res.send(200, bookings);
+    })
+    app.get('/booking/id/:id', (req, res) => {
+      console.log(req.params.id)
+
+      if(bookings[req.params.id]){
+        res.send(200, bookings[req.params.id]);
+      } else {
+        res.send(404, "404: no booking found")
+      }
+
+    })
 };
